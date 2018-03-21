d0c0691b7c7fc08cdc65acc34803162753511e2e
james <19514687+jrondina@users.noreply.github.com>
2018-03-20 23:26:29 -0400

    half solution for electronic shop

---
+++ algorithms/implementation/electronicsshop.py
@@ -0,0 +1,29 @@
+# https://www.hackerrank.com/challenges/electronics-shop/problem
+
+
+def getMoneySpent(keyboards, drives, s):
+    total = 0
+    if len(keyboards) < len(drives):
+        for x in keyboards:
+            for y in list(filter(lambda z: z >= min(drives) and z < s - x, drives)):
+                if x + y == s:
+                    return s
+                elif x + y > s:
+                    pass
+                elif s > x + y > total:
+                    total = x + y
+                else:
+                    pass
+        return total
+
+
+    print(keyboards, drives)
+
+
+s,n,m = input().strip().split(' ')
+s,n,m = [int(s),int(n),int(m)]
+keyboards = list(map(int, input().strip().split(' ')))
+drives = list(map(int, input().strip().split(' ')))
+#  The maximum amount of money she can spend on a keyboard and USB drive, or -1 if she can't purchase both items
+moneySpent = getMoneySpent(keyboards, drives, s)
+print(moneySpent)
